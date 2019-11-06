db286a53c6738d56973a4f5b32b919e372b6eeaa
kaiweiovo <kaiweiovo@163.com>
2019-11-06 18:52:59 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -5,7 +5,113 @@ $(function(){
         || document.documentElement.scrolltop
         || 0;
 
-    
+    var $daily = $('#daily');
+    $('#canvas').attr({width: $daily.width(),height: $daily.height()-70});
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    let imgs = [
+    {
+        image: new Image(),
+        src:'./resource/img/1.gif',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/2.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/3.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/4.png',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/5.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/6.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/7.gif',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/8.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/9.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/10.png',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/11.png',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/12.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/13.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/14.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/15.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/16.gif',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/17.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/18.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/19.gif',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/20.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/21.jpg',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/22.gif',
+    },
+    {
+        image: new Image(),
+        src:'./resource/img/23.gif',
+    }];
+
+    imgs.map(function(img,index,arr){
+        img.image.src = img.src;
+        img.x = 0;
+        img.y = 100 + Math.random()*(canvas.height-300);
+        img.angle = Math.random();
+        img.deg = 0;
+        img.delay = Math.random() * 6000;
+        img.show = true;
+    })
 
     if(scrolltop >= $('#subjects').offset().top)
         $('#nav-container').addClass('nav-slide-down');
@@ -57,89 +163,6 @@ $(function(){
     }
 
     var dailyAnimation = once(function(){
-        var $daily = $('#daily');
-        $('#canvas').attr({width: $daily.width(),height: $daily.height()-70});
-        let canvas = document.getElementById('canvas');
-        let ctx = canvas.getContext('2d');
-        let imgs = [
-        {
-            src:'./resource/img/1.gif',
-        },
-        {
-            src:'./resource/img/2.jpg',
-        },
-        {
-            src:'./resource/img/3.jpg',
-        },
-        {
-            src:'./resource/img/4.png',
-        },
-        {
-            src:'./resource/img/5.jpg',
-        },
-        {
-            src:'./resource/img/6.jpg',
-        },
-        {
-            src:'./resource/img/7.gif',
-        },
-        {
-            src:'./resource/img/8.jpg',
-        },
-        {
-            src:'./resource/img/9.jpg',
-        },
-        {
-            src:'./resource/img/10.png',
-        },
-        {
-            src:'./resource/img/11.png',
-        },
-        {
-            src:'./resource/img/12.jpg',
-        },
-        {
-            src:'./resource/img/13.jpg',
-        },
-        {
-            src:'./resource/img/14.jpg',
-        },
-        {
-            src:'./resource/img/15.jpg',
-        },
-        {
-            src:'./resource/img/16.gif',
-        },
-        {
-            src:'./resource/img/17.jpg',
-        },
-        {
-            src:'./resource/img/18.jpg',
-        },
-        {
-            src:'./resource/img/19.gif',
-        },
-        {
-            src:'./resource/img/20.jpg',
-        },
-        {
-            src:'./resource/img/21.jpg',
-        },
-        {
-            src:'./resource/img/22.gif',
-        },
-        {
-            src:'./resource/img/23.gif',
-        }];
-
-        imgs.map(function(img,index,arr){
-            img.x = 0;
-            img.y = 100 + Math.random()*(canvas.height-300);
-            img.angle = Math.random();
-            img.deg = 0;
-            img.delay = Math.random() * 6000;
-            img.show = true;
-        })
         var times = 0;
         setTimeout(() => {
             $('.me').show();
@@ -152,15 +175,13 @@ $(function(){
             {
                 if(imgs[i].delay <= times && imgs[i].show)
                 {
-                    var img = new Image()
-                    img.src = imgs[i].src;
                     ctx.save();
                     ctx.translate(imgs[i].x,imgs[i].y);
                     ctx.beginPath();
                     ctx.arc(0,0,50,0,4 * Math.PI);
                     ctx.clip();
                     ctx.rotate(imgs[i].deg * Math.PI / 180);
-                    ctx.drawImage(img,-50,-50,100,100);
+                    ctx.drawImage(imgs[i].image,-50,-50,100,100);
                     ctx.restore();
                     imgs[i].x+=10;
                     imgs[i].deg+=imgs[i].angle;
