0f05b65bdfb6c8d66118146ecd73952f6ab1f3c5
kaiweiovo <kaiweiovo@163.com>
2019-11-06 17:38:42 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ $(function(){
         || document.documentElement.scrolltop
         || 0;
 
+    
+
     if(scrolltop >= $('#subjects').offset().top)
         $('#nav-container').addClass('nav-slide-down');
 
@@ -42,6 +44,135 @@ $(function(){
         }, 400);
     });
 
+    function once(fn){
+        var result;
+        return function(){
+            if(fn)
+            {
+                result = fn.apply(this,arguments);
+                fn = null;
+            }
+            return result;
+        };
+    }
+
+    var dailyAnimation = once(function(){
+        var $daily = $('#daily');
+        $('#canvas').attr({width: $daily.width(),height: $daily.height()-70});
+        let canvas = document.getElementById('canvas');
+        let ctx = canvas.getContext('2d');
+        let imgs = [
+        {
+            src:'./resource/img/1.gif',
+        },
+        {
+            src:'./resource/img/2.jpg',
+        },
+        {
+            src:'./resource/img/3.jpg',
+        },
+        {
+            src:'./resource/img/4.png',
+        },
+        {
+            src:'./resource/img/5.jpg',
+        },
+        {
+            src:'./resource/img/6.jpg',
+        },
+        {
+            src:'./resource/img/7.gif',
+        },
+        {
+            src:'./resource/img/8.jpg',
+        },
+        {
+            src:'./resource/img/9.jpg',
+        },
+        {
+            src:'./resource/img/10.png',
+        },
+        {
+            src:'./resource/img/11.png',
+        },
+        {
+            src:'./resource/img/12.jpg',
+        },
+        {
+            src:'./resource/img/13.jpg',
+        },
+        {
+            src:'./resource/img/14.jpg',
+        },
+        {
+            src:'./resource/img/15.jpg',
+        },
+        {
+            src:'./resource/img/16.gif',
+        },
+        {
+            src:'./resource/img/17.jpg',
+        },
+        {
+            src:'./resource/img/18.jpg',
+        },
+        {
+            src:'./resource/img/19.gif',
+        },
+        {
+            src:'./resource/img/20.jpg',
+        },
+        {
+            src:'./resource/img/21.jpg',
+        },
+        {
+            src:'./resource/img/22.gif',
+        },
+        {
+            src:'./resource/img/23.gif',
+        }];
+
+        imgs.map(function(img,index,arr){
+            img.x = 0;
+            img.y = 100 + Math.random()*(canvas.height-300);
+            img.angle = Math.random();
+            img.deg = 0;
+            img.delay = Math.random() * 6000;
+            img.show = true;
+        })
+        var times = 0;
+        setTimeout(() => {
+            $('.me').show();
+            $('.me').animate({opacity: 1},'slow');
+            $('.canvas').css({filter: blur(10)});
+        }, 6000);
+        var interval = setInterval(() => {
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            for(var i=0;i<imgs.length;i++)
+            {
+                if(imgs[i].delay <= times && imgs[i].show)
+                {
+                    var img = new Image()
+                    img.src = imgs[i].src;
+                    ctx.save();
+                    ctx.translate(imgs[i].x,imgs[i].y);
+                    ctx.beginPath();
+                    ctx.arc(0,0,50,0,4 * Math.PI);
+                    ctx.clip();
+                    ctx.rotate(imgs[i].deg * Math.PI / 180);
+                    ctx.drawImage(img,-50,-50,100,100);
+                    ctx.restore();
+                    imgs[i].x+=10;
+                    imgs[i].deg+=imgs[i].angle;
+                }
+                if(imgs[i].x >= canvas.width + 50)
+                    imgs[i].show = false;
+            }
+            times += 16;
+        }, 16); 
+
+    });
+
     $(window).on('scroll',function(){
 
         scrolltop = window.pageYOffset
@@ -59,20 +190,21 @@ $(function(){
             if(scrolltop + window.innerHeight >= $(this).offset().top)
                 $(this).addClass('liSlide');
         })
+
+        if(scrolltop >= $('#daily').offset().top)
+            dailyAnimation();
     })
     
-    var $daily = $('#daily');
-    $('#canvas').attr({width: $daily.width(),height: $daily.height()});
+    if(scrolltop >= $('#daily').offset().top)
+            dailyAnimation();
 
-        let ctx;
-        let haski;
-        haski = new Image();
-        haski.src = './resource/img/haski.gif';
-        let canvas = document.getElementById('canvas');
-        ctx = canvas.getContext('2d');
-        ctx.drawImage(haski,0,0,300,300);
-        ctx.save();
-        ctx.translate(150,150);
 
-    
+    $('.folder').hover(
+        function(){
+            $(this).addClass('folder-hover');
+        },
+        function(){
+            $(this).removeClass('folder-hover');
+    })
+
 })
